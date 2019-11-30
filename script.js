dc6c36d3dba637687370d01d1c118ea80a43092e
eido <48271387+eido8@users.noreply.github.com>
2019-11-30 13:51:40 +0900

    Added JS and minor changes in design

---
+++ script.js
@@ -0,0 +1,105 @@
+window.onload = function() {
+
+// HOME TAB
+    setTimeout(
+        function() {
+            document.querySelector(".intro-message").style.opacity = "1";
+        }, 100
+    );
+
+    setTimeout(
+        function() {
+            document.querySelector(".intro-description").style.opacity = "1";
+        }, 800
+    );
+
+    setTimeout(
+        function() {
+            document.querySelector(".intro-browse").style.opacity = "1";
+        }, 800
+    );
+
+// ABOUT TAB
+
+    setTimeout(
+        function() {
+            document.querySelector(".about-jful").style.opacity = "1";
+        }, 100
+    );
+
+    setTimeout(
+        function() {
+            document.querySelector(".bio").style.opacity = "1";
+        }, 500
+    );
+
+    setTimeout(
+        function() {
+            document.querySelector(".donate-text").style.opacity = "1";
+        }, 1600
+    );
+
+// BROWSE TAB
+    setTimeout(
+        function() {
+            document.querySelector(".jful-x-eido-collection").style.opacity = "1";
+        }, 100
+    );
+
+    setTimeout(
+        function() {
+            document.querySelector(".browse-thru").style.opacity = "1";
+        }, 800
+    );
+
+    setTimeout(
+        function() {
+            document.querySelector(".browse-note").style.opacity = "1";
+        }, 800
+    );
+
+// PRICING INFORMATION
+    setTimeout(
+        function() {
+            document.querySelector(".pricing-information").style.opacity = "1";
+        }, 100
+    );
+
+    setTimeout(
+        function() {
+            document.querySelector(".purchase-note").style.opacity = "1";
+        }, 800
+    );
+
+    setTimeout(
+        function() {
+            document.querySelector("h3.instructions").style.opacity = "1";
+        }, 1600
+    );
+
+    setTimeout(
+        function() {
+            document.querySelector("h1.instructions").style.opacity = "1";
+        }, 1600
+    );
+
+    setTimeout(
+        function() {
+            document.querySelector(".row").style.opacity = "1";
+        }, 1600
+    );
+
+    setTimeout(
+        function() {
+            document.querySelector(".paypal-icon").style.opacity = "1";
+        }, 1600
+    );
+
+
+// CONTACT SECTION
+    setTimeout(
+        function() {
+            document.querySelector(".contact-info").style.opacity = "1";
+        }, 1600
+    );
+}
