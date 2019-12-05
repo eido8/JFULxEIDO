d32eca526bf6f0787e8be365e7a809d81ad78bdf
eido <48271387+eido8@users.noreply.github.com>
2019-12-06 00:17:17 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,64 @@
 // BROWSE
+// TOGGLE TRACK PRICING
+document.getElementById("add-meant-to-be").addEventListener("click", toggleMTB);
 
-document.getElementById("add-meant-to-be").onclick = function() {
-    document.getElementById("meant-to-be-container").style.cssText = "display: none";
-    document.getElementById("meant-to-be-overlay").style.cssText = "display: block";
+function toggleMTB() {  
+    var trackName = document.getElementById("meant-to-be-container");
+    var priceOverlay = document.getElementById("meant-to-be-overlay");
+    var displayType = window.getComputedStyle(trackName).display;
+
+    if (displayType == "block") {
+        trackName.style.display = "none";
+        priceOverlay.style.display = "block";
+    } else {
+        trackName.style.display = "block";
+        priceOverlay.style.display = "none";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+document.getElementById("add-you-want").addEventListener("click", toggleYW);
+
+function toggleYW() {  
+    var trackName = document.getElementById("you-want-container");
+    var priceOverlay = document.getElementById("you-want-overlay");
+    var displayType = window.getComputedStyle(trackName).display;
+
+    if (displayType == "block") {
+        trackName.style.display = "none";
+        priceOverlay.style.display = "block";
+    } else {
+        trackName.style.display = "block";
+        priceOverlay.style.display = "none";
+    }
+}
+
+
+
+
+
+document.getElementById("add-city-sounds").addEventListener("click", toggleCS);
+
+function toggleCS() {  
+    var trackName = document.getElementById("city-sounds-container");
+    var priceOverlay = document.getElementById("city-sounds-overlay");
+    var displayType = window.getComputedStyle(trackName).display;
+
+    if (displayType == "block") {
+        trackName.style.display = "none";
+        priceOverlay.style.display = "block";
+    } else {
+        trackName.style.display = "block";
+        priceOverlay.style.display = "none";
+    }
+}
+
 
